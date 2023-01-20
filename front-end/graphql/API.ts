5bb74ce28ed9d4ef0162275ba7e07fb49801ab08
dingdalton <daltonhtx@gmail.com>
2023-01-20 17:04:57 -0600

    Update API.ts

--- front-end/graphql/API.ts
+++ front-end/graphql/API.ts
@@ -2,4 +2,16 @@
 /* eslint-disable */
 //  This file was automatically generated and should not be edited.
 
-export {}
+export type CognitoUser = {
+    __typename: "CognitoUser",
+    id: string,
+    username: string,
+  };
+  
+  export type GetCognitoUserQuery = {
+    getCognitoUser?:  {
+      __typename: "CognitoUser",
+      id: string,
+      username: string,
+    } | null,
+  };
